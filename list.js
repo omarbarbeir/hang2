1ed2efefcf2933844012d740e5cfd652ca928f9d
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-09-04 00:01:37 +0300

    Add files via upload

---
+++ list.js
@@ -0,0 +1,101 @@
+let list = [
+{
+    word:"newzealand",
+    hint: "The place where the Lord of the Rings was filmed"
+},
+
+{   word: "starbucks cup",
+    hint:" item in every fight club scene"
+},
+
+{
+    word: "woody",
+    hint:" character in the Toy Story movies  voiced by Tom Hanks"
+},
+
+{
+    word: "emmawatson",
+    hint:" played Hermione in the Harry Potter movies"
+},
+
+{
+    word: "hogwarts",
+    hint:" name of the school featured in the Harry Potter movies"
+},
+
+{
+    word: "pele",
+    hint:" youngest player to ever score in a World Cup"
+},
+
+{
+    word: "arsenal",
+    hint:" club has won the most FA Cup titles"
+},
+
+{
+    word: "valetta",
+    hint:" capital of Malta"
+},
+
+{
+    word: "bennevis",
+    hint:" highest mountain in Britain"
+},
+
+{
+    word: "lima",
+    hint: "capital of Peru"
+},
+
+{
+    word: "krona",
+    hint: "currency of Sweden"
+},
+
+{
+    word: "severn",
+    hint:"longest river in the UK"
+},
+
+{
+    word:"greenland",
+    hint: "biggest island in the world"
+},
+
+{
+    word:"jupiter",
+    hint: "biggest planet in our solar system"
+},
+
+{
+    word:"johnadams",
+    hint: "second President of the United States"
+},
+
+{
+    word:"lily",
+    hint: "name of Harry potter mother"
+},
+
+{
+    word:"nagini",
+    hint: "Voldermort's pet snake"
+},
+{
+    word: "guitar",
+    hint: "A musical instrument with strings"
+},
+{
+    word: "oxygen",
+    hint: "A colorless, odorless gas essential for life"
+},
+{
+    word: "mountain",
+    hint: "A larg natural elevation of the Earth surface"
+},
+{
+    word: "painting",
+    hint: "An art from using colors on a surface to create images of expression"
+},
+]
